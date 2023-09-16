3a06039956719690f2969ea055296950b5bb1881
Deon <deeyonn@gmail.com>
2023-09-16 13:00:15 +0800

    config(next.config.js): Add basePath
    - Add basePath in config so that CSS and JS can load

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export",
+  basePath: "/ay2324s1-course-assessment-g25",
 }
 
 module.exports = nextConfig
