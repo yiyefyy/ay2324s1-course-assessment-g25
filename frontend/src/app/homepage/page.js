bce9437268ab8c5ec8ce0992585e498069658ae5
wxy1203 <e0817822@u.nus.edu>
2023-09-19 19:21:44 +0800

    Change some file config

--- frontend/src/app/homepage/page.js
+++ frontend/src/app/homepage/page.js
@@ -5,6 +5,8 @@ import * as AiIcons from 'react-icons/ai';
 import * as FaIcons from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 import { SideBar } from '../../components/sideBar';
+//import 'frontend/src/app/styles.css';
+
 
 export default function Homepage() {
     const [sidebar, setsidebar] = useState(false);
@@ -50,7 +52,104 @@ export default function Homepage() {
                     </nav>
                 </div>
             </IconContext.Provider>
-            <p className="p-7 text-lg font-semibold mb-2">Welcome back, {storedName}</p>
+            <p className="p-7 text-lg font-semibold mb-2">Welcome to PeerPrep, {storedName}</p>
+            <main>
+                <h2>Add a new question</h2>
+                <div className="form-center">
+                    <form id="questionForm">
+                    <label htmlFor="questionTitle">Title:</label>
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        id="questionTitle"
+                        name="questionTitle"
+                        autoComplete="off"
+                        maxLength="100"
+                    />
+                    <br /><br />
+                    <label htmlFor="questionDescription">Description:</label>
+                    <textarea
+                        placeholder="Description"
+                        id="questionDescription"
+                        name="questionDescription"
+                        wrap="soft"
+                        maxLength="700"
+                    ></textarea>
+                    <br /><br />
+                    <label htmlFor="questionComplexity">Complexity:</label>
+                    <select name="questionComplexity" id="questionComplexity">
+                        <option value="Easy">Easy</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Hard">Hard</option>
+                    </select>
+                    <br /><br />
+                    <label htmlFor="questionCategory">Category:</label>
+                    <input
+                        type="text"
+                        placeholder="Category"
+                        id="questionCategory"
+                        name="questionCategory"
+                        autoComplete="off"
+                        maxLength="100"
+                    />
+                    <br /><br />
+                    <div className="button-container">
+                        <button type="submit">Add Question</button>
+                        <button id="clearAllButton" type="button">Clear All</button>
+                    </div>
+                    </form>
+                </div>
+
+                {/* Add a modal for confirmation */}
+                <div id="confirmationModal" className="modal">
+                    <div className="modal-content">
+                    <p>Are you sure you want to delete this question?</p>
+                    <button id="confirmDeleteButton">Yes</button>
+                    <button id="cancelDeleteButton">No</button>
+                    </div>
+                </div>
+
+                <div className="table-conatiner">
+                    <table>
+                    <caption>
+                        Questions
+                    </caption>
+                    <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Complexity</th>
+                        <th>Category</th>
+                        <th>Delete</th>
+                        </tr>
+                    </thead>
+                    {/* This is where questions will be dynamically added */}
+                    <tr>
+                        {/* <td className="question-id">${question.id}</td>
+                        <td className="question-title">${question.title}</td>
+                        <td>${question.complexity}</td>
+                        <td>${question.category}</td> */}
+                        <td>
+                        <button className="delete-button" data-index="${index}">Delete</button>
+                        </td>
+                    </tr>
+                    </table>
+                </div>
+
+                <div className="question-details" id="questionDetailsContainer">
+                    {/* Details of the selected question will be displayed here */}
+                </div>
+
+                {/* Button to check local storage content */}
+                <div className="button-container">
+                    <button id="checkLocalStorageButton">Check Local Storage in Console</button>
+                </div>
+            </main>
         </div>
+
+        
+        
+
+
     )
 }
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
+// - Run `npm test` to run the tests
+
 module.exports = {
     testMatch: ['**/frontend/test/**/*.js', '**/?(*.)+(spec|test).js'],
     testEnvironment: 'node', // For testing in a browser-like environment
   };
-  
