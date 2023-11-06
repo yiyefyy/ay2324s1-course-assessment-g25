1a64de5e848e0594773b01cfe1ca6bf31d00cec7
wxy1203 <e0817822@u.nus.edu>
2023-11-06 13:13:31 +0800

    Make the loading icon in the center of code editor

--- frontend/components/Loading.tsx
+++ frontend/components/Loading.tsx
@@ -3,7 +3,9 @@ import styles from "./Loading.module.css";
 export function Loading() {
   return (
     <div className={styles.loading}>
-      <img src="https://liveblocks.io/loading.svg" alt="Loading" />
+        <div className='w-5/12 h-1/4'>
+          <img src="https://liveblocks.io/loading.svg" alt="Loading" />
+        </div>
     </div>
   );
 }
