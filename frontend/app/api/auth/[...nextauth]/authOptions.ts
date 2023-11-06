55595b5e9efbdecd2488387400aa7e4acb2e1c5b
Deon <deeyonn@gmail.com>
2023-11-06 16:03:28 +0800

    fix(authOptions.js): Remove ability to login using email

--- frontend/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,8 +1,10 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-/* import { PrismaClient } from "@prisma/client"; */
-import type { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import type { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+/* import { PrismaClient } from "@prisma/client"; */
+
+
 
 const{PrismaClient} = require("@prisma/client")
 
@@ -16,43 +18,43 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-          placeholder: "your-cool-email"
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "your-awesome-password"
-        }
-      },
-      async authorize(credentials, req) {
-        // This function manages authorsation for the CredentialsProvider.
-        // credentials is defined above, and req contains additional information,
-        // such as the ip address of the user.
-        const email = credentials?.email;
-        const password = credentials?.password;
-        console.log(`Received credentials: ${email} ${password} from ${req?.headers?.host}`)
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     email: {
+    //       label: "Email",
+    //       type: "text",
+    //       placeholder: "your-cool-email"
+    //     },
+    //     password: {
+    //       label: "Password",
+    //       type: "password",
+    //       placeholder: "your-awesome-password"
+    //     }
+    //   },
+    //   async authorize(credentials, req) {
+    //     // This function manages authorsation for the CredentialsProvider.
+    //     // credentials is defined above, and req contains additional information,
+    //     // such as the ip address of the user.
+    //     const email = credentials?.email;
+    //     const password = credentials?.password;
+    //     console.log(`Received credentials: ${email} ${password} from ${req?.headers?.host}`)
 
-        // If the user doesn't provide an email or password, return null.
-        if (!email || !password) return null;
+    //     // If the user doesn't provide an email or password, return null.
+    //     if (!email || !password) return null;
 
-        // Otherwise, check the database for a user with the provided email and password.
-        const user = await prisma.user.findUnique({
-          where: { email: email }
-        })
+    //     // Otherwise, check the database for a user with the provided email and password.
+    //     const user = await prisma.user.findUnique({
+    //       where: { email: email }
+    //     })
 
-        if (!user) return null;
+    //     if (!user) return null;
 
-        // If the user exists, check the password.
-        // Currently does not check for password, looking for a workaround.
-        return user
-      }
-    })
+    //     // If the user exists, check the password.
+    //     // Currently does not check for password, looking for a workaround.
+    //     return user
+    //   }
+    // })
   ],
   session: {
     strategy: "jwt",
