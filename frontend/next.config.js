13137aff1717874daee34bbf17d3297e27601961
deeyonn <deeyonn@gmail.com>
2023-11-09 12:28:30 +0800

    fix(next.config.js): Fix module not found error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
   basePath: pathPrefix,
   // output: "export",
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
