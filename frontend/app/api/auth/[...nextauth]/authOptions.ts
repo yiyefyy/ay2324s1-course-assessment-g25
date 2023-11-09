4755f2c7646c3d727989a74eb2d4789d169b4b2d
deeyonn <deeyonn@gmail.com>
2023-11-09 12:22:56 +0800

    revert(authOptions): Add email auth

--- frontend/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
 import GithubProvider from "next-auth/providers/github";
 
@@ -12,7 +13,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       profile(profile) {
-        return { role: profile.role ?? "user" }
+        return { role: profile.role ?? "user", ...profile }
       },
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
@@ -21,51 +22,51 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.DISCORD_ID as string,
       clientSecret: process.env.DISCORD_SECRET as string,
     }),
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   credentials: {
-    //     email: {
-    //       label: "Email",
-    //       type: "text",
-    //       placeholder: "your-cool-email"
-    //     },
-    //     password: {
-    //       label: "Password",
-    //       type: "password",
-    //       placeholder: "your-awesome-password"
-    //     }
-    //   },
-    //   async authorize(credentials, req) {
-    //     // This function manages authorsation for the CredentialsProvider.
-    //     // credentials is defined above, and req contains additional information,
-    //     // such as the ip address of the user.
-    //     const email = credentials?.email;
-    //     const password = credentials?.password;
-    //     console.log(`Received credentials: ${email} ${password} from ${req?.headers?.host}`)
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "your-cool-email"
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "your-awesome-password"
+        }
+      },
+      async authorize(credentials, req) {
+        // This function manages authorsation for the CredentialsProvider.
+        // credentials is defined above, and req contains additional information,
+        // such as the ip address of the user.
+        const email = credentials?.email;
+        const password = credentials?.password;
+        console.log(`Received credentials: ${email} ${password} from ${req?.headers?.host}`)
 
-    //     // If the user doesn't provide an email or password, return null.
-    //     if (!email || !password) return null;
+        // If the user doesn't provide an email or password, return null.
+        if (!email || !password) return null;
 
-    //     // Otherwise, check the database for a user with the provided email and password.
-    //     const user = await prisma.user.findUnique({
-    //       where: { email: email }
-    //     })
+        // Otherwise, check the database for a user with the provided email and password.
+        const user = await prisma.user.findUnique({
+          where: { email: email }
+        })
 
-    //     if (!user) return null;
+        if (!user) return null;
 
-    //     // If the user exists, check the password.
-    //     // Currently does not check for password, looking for a workaround.
-    //     return user
-    //   }
-    // })
+        // If the user exists, check the password.
+        // Currently does not check for password, looking for a workaround.
+        return user
+      }
+    })
   ],
   session: {
     strategy: "jwt",
   },
   callbacks: {
-    async session({session, user}) {
+    async session({ session, user }: { session: any, user: any }) {
       // Add role to session.
-      session.user?.role = user.role;
+      session.user.role = user.role;
       return session;
     }
   },
